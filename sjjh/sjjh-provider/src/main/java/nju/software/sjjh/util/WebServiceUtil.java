cff561df18058fe375e7b961b96045e7b971b66b
nekonekod <nekonekod.goo@gmail.com>
2017-04-26 18:38:51 +0800

    ws工具类

--- sjjh/sjjh-provider/src/main/java/nju/software/sjjh/util/WebServiceUtil.java
+++ sjjh/sjjh-provider/src/main/java/nju/software/sjjh/util/WebServiceUtil.java
@@ -1,6 +1,8 @@
 package nju.software.sjjh.util;
 
 import lombok.extern.slf4j.Slf4j;
+import nju.software.sjjh.exception.BaseAppException;
+import nju.software.sjjh.exception.RemoteException;
 import org.apache.axis.client.Call;
 import org.apache.axis.client.Service;
 import org.apache.axis.encoding.XMLType;
@@ -30,7 +32,7 @@ public class WebServiceUtil {
     }
 
     private static String invoke(String address, String soapAction, String funName, String[] paramNames, String[] paramValues) {
-        log.warn("监狱webservice：" + funName);
+        log.warn("调用webservice：" +address + "," + funName);
         try {
             Service s = new Service();
             Call call = (Call) s.createCall();
@@ -43,15 +45,16 @@ public class WebServiceUtil {
             }
             return (String) call.invoke(paramValues);
         } catch (Exception e) {
-            log.error("调用webservice失败:" + Arrays.toString(new String[]{
-                    "address:"+address,
-                    "soapAction:"+soapAction,
-                    "funName:"+funName,
-                    "paramNames:"+Arrays.toString(paramNames),
-                    "paramValues:"+Arrays.toString(paramValues),
-            }), e);
+            String err = "调用webservice失败:" + Arrays.toString(new String[]{
+                            "address:"+address,
+                            "soapAction:"+soapAction,
+                            "funName:"+funName,
+                            "paramNames:"+Arrays.toString(paramNames),
+                            "paramValues:"+Arrays.toString(paramValues),
+                    });
+            log.error(err, e);
+            throw new RemoteException(err,e);
         }
-        return null;
     }
 
 }
